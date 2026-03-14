14e8725e779f284eac99d7c8efb85973167f4223
Mark raouf <markraouf2001@gmail.com>
2026-03-14 13:25:36 +0200

    feat(auth): fix issue round 4

--- lib/features/auth/sign_in/presentation/pages/sign_in_screen.dart
+++ lib/features/auth/sign_in/presentation/pages/sign_in_screen.dart
@@ -9,6 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../../../core/utils/validators.dart';
+import '../../../../Home/presentation/pages/home_screen.dart';
 
 class SignInScreen extends StatelessWidget {
   SignInViewModel _signInViewModel = getIt<SignInViewModel>();
